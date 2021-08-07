a5038597eecb19d6b9ee2c15a652756f259730fa
Hadeel Ghourani <hadeelghourani@gmail.com>
2021-08-07 13:32:23 +0300

    New Eddition

--- DataStructure/src/main/java/com/mycompany/datastructure/Node.java
+++ DataStructure/src/main/java/com/mycompany/datastructure/Node.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.datastructure;
 
-/**
- *
- * @author hadee
- */
 public class Node {
-    
+    Node next;
+    int data;
+    Node (int data){
+        this.data = data;
+        next = null;
+    }
 }
--- DataStructure/src/main/java/com/mycompany/datastructure/SinglyLinkedList.java
+++ DataStructure/src/main/java/com/mycompany/datastructure/SinglyLinkedList.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.datastructure;
 
-/**
- *
- * @author hadee
- */
 public class SinglyLinkedList {
+    Node head;
     
+    public void insert (int data){
+        if (head == null){
+            head = new Node(data);
+        }
+        else {
+            Node temp = head;
+            while (temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = new Node(data);
+        }
+    }
+    
+    public void printLinkedList(SinglyLinkedList l){
+        Node temp=head;
+        while(temp.next != null){
+            System.out.print(temp.data + " --> ");
+            temp = temp.next;
+        }
+        System.out.println(temp.data);
+    
+    }
 }
